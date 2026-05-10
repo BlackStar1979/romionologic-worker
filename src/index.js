78681b6afb5077968f70847eab898917ba667547
BlackStar1979 <blackstar1979@gmail.com>
2026-05-10 21:19:52 +0200

    chore: rename worker to romionologic + improve package.json + clean index.js

--- src/index.js
+++ src/index.js
@@ -1,23 +1,8 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
 
-    // Główna strona → zwracamy index.html z folderu public
-    if (url.pathname === "/" || url.pathname === "") {
-      return env.ASSETS.fetch(request);
-    }
-
-    // Wszystkie inne pliki statyczne (css, js, obrazy itp.)
+    // Serwuj pliki statyczne z folderu ./static
     return env.ASSETS.fetch(request);
   },
 };
